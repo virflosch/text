222849ee75e495ddd92298bf7ee705be9a2db709
virflosch <yaoyao12055@qq.com>
2026-04-03 15:17:12 +0800

    Update tool.h

--- tool.h
+++ tool.h
@@ -2,21 +2,10 @@
 #define TOOL_H
 
 #include "global.h"
-#include <time.h>
 
-// 清屏函数
 void clearScreen();
-
-// 暂停函数（等待按键）
 void pauseProgram();
+void getCurrentTimeStr(char* str);
+time_t stringToTime(char* pTime);
 
-// 获取当前时间字符串（输出到time_str）
-void getCurrentTime(char* time_str);
-
-// 输入卡号（带长度校验）
-void inputCardId(char* card_id);
-
-// 输入密码（带长度校验）
-void inputPassword(char* pwd);
-
-#endif // TOOL_H
+#endif
