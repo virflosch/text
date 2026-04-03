2794f379d1ff2fad1eef1a99fd899f50177127c1
virflosch <yaoyao12055@qq.com>
2026-04-03 15:16:57 +0800

    Update model.h

--- model.h
+++ model.h
@@ -3,10 +3,9 @@
 
 #include "global.h"
 
-// 链表结点结构体
 typedef struct CardNode {
-    Card data;               // 数据区：卡信息
-    struct CardNode* next;   // 指针区：指向下一个结点
+    Card data;
+    struct CardNode* next;
 } CardNode, * lpCardNode;
 
-#endif // MODEL_H
+#endif
