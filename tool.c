0f02f1a9dbb829f0f5ca253e79d8f86eb61b7cd6
virflosch <yaoyao12055@qq.com>
2026-04-03 15:19:56 +0800

    Update tool.c

--- tool.c
+++ tool.c
@@ -2,53 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-// 清屏（Windows/Linux兼容）
-void clearScreen() {
-#ifdef _WIN32
-    system("cls");
-#else
-    system("clear");
-#endif
-}
+void clearScreen() { system("cls"); }
+void pauseProgram() { system("pause"); }
 
-// 暂停程序
-void pauseProgram() {
-#ifdef _WIN32
-    system("pause");
-#else
-    printf("按任意键继续...");
-    getchar();
-#endif
-}
-
-// 获取当前时间（格式：2026-03-13 16:30）
-void getCurrentTime(char* time_str) {
+void getCurrentTimeStr(char* str) {
     time_t t = time(NULL);
-    struct tm* tm_info = localtime(&t);
-    strftime(time_str, 20, "%Y-%m-%d %H:%M", tm_info);
+    struct tm* lt = localtime(&t);
+    strftime(str, TIME_LEN, "%Y-%m-%d %H:%M", lt);
 }
 
-// 输入卡号（校验1~18位）
-void inputCardId(char* card_id) {
-    while (1) {
-        printf("请输入查询的卡号(长度为1~18)：");
-        scanf("%s", card_id);
-        if (strlen(card_id) >= 1 && strlen(card_id) <= CARD_ID_LEN) {
-            break;
-        }
-        printf("卡号长度必须在1~18之间！\n");
-    }
-}
-
-// 输入密码（校验1~8位）
-void inputPassword(char* pwd) {
-    while (1) {
-        printf("请输入密码(长度为1~8)：");
-        scanf("%s", pwd);
-        if (strlen(pwd) >= 1 && strlen(pwd) <= PWD_LEN) {
-            break;
-        }
-        printf("密码长度必须在1~8之间！\n");
-    }
+time_t stringToTime(char* pTime) {
+    struct tm tm = { 0 };
+    sscanf(pTime, "%d-%d-%d %d:%d",
+        &tm.tm_year, &tm.tm_mon, &tm.tm_mday,
+        &tm.tm_hour, &tm.tm_min);
+    tm.tm_year -= 1900;
+    tm.tm_mon -= 1;
+    return mktime(&tm);
 }
