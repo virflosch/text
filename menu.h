3dac85afa71c38a47ef3aa3929f7236ba6f2022d
virflosch <yaoyao12055@qq.com>
2026-04-03 15:16:38 +0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,16 +1,9 @@
 #ifndef MENU_H
 #define MENU_H
 
-// 显示主菜单
 void showMainMenu();
-
-// 添加卡界面
 void addCardUI();
-
-// 精确查询卡界面
 void queryCardUI();
+void fuzzyQueryUI();
 
-// 模糊查询卡界面（新增）
-void fuzzyQueryCardUI();
-
-#endif // MENU_H
+#endif
